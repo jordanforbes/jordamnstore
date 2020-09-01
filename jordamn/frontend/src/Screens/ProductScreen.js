cbd2a6d69b5fdd54cdcec874e9c4a215dff51625
jordanforbes <jordforb@gmail.com>
2020-08-31 20:37:43 -0400

    finished basic details screen

--- jordamn/frontend/src/Screens/ProductScreen.js
+++ jordamn/frontend/src/Screens/ProductScreen.js
@@ -5,8 +5,8 @@ import {Link} from 'react-router-dom';
 const ProductScreen = (props) => {
     console.log(props.match.params.id)
     const product = data.products.find(x=> x._id === props.match.params.id);
-    return <div className=""> 
-        <div>
+    return <div> 
+        <div className="back-to-result">
             <Link to="/">Back to result</Link>
         </div>
         <div className="details">
@@ -20,6 +20,37 @@ const ProductScreen = (props) => {
                     </li>
                     <li>
                         {product.rating} Stars ({product.numReviews} Reviews)
+                    </li>
+                    <li>
+                        Price: <b>${product.price}</b>
+                    </li>
+                    <li>
+                        Description:
+                        <div>
+                            {product.description}
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div className="details-action">
+                <ul>
+                    <li>
+                        Price: {product.price}
+                    </li>
+                    <li>
+                        Status: {product.status}
+                    </li>
+                    <li>
+                        Qty:
+                        <select>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                        </select>
+                    </li>
+                    <li>
+                        <button className="button">Add to Cart</button>
                     </li>
                 </ul>
             </div>
